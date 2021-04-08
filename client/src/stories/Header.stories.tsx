6aeca116b7796afd435ca30e42d66914ef4146cd
TannerHolladay <tannerholladay6@gmail.com>
2021-04-08 13:25:55 -0600

    Fixed eslint issues with storybook

--- client/src/stories/Header.stories.tsx
+++ client/src/stories/Header.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import { Story } from '@storybook/react';
 
 import { Header, HeaderProps } from './Header';
 
 export default {
   title: 'Example/Header',
   component: Header,
-} as Meta;
+};
 
 const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 
