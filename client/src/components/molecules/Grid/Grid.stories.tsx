2a423ada201acc8f5eb986f4620dd79ec4d452a2
TannerHolladay <tannerholladay6@gmail.com>
2021-05-05 13:06:00 -0600

    Seperated panel component from grid component

--- client/src/components/molecules/Grid/Grid.stories.tsx
+++ client/src/components/molecules/Grid/Grid.stories.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 
-import { GridContainer } from './GridContainer';
+import { Grid } from './Grid';
 
 export default {
-  title: 'Organisms/GridContainer',
-  component: GridContainer,
+  title: 'Molecules/Grid',
+  component: Grid,
 };
 
 export const Default: Story = () =>
-  <GridContainer title="Default">
+  <Grid title="Default">
     <div key={1} data-grid={{ x: 0, y: 0, w: 1, h: 1 }} />
     <div key={2} data-grid={{ x: 1, y: 0, w: 1, h: 1 }} />
     <div key={3} data-grid={{ x: 2, y: 0, w: 1, h: 1 }} />
     <div key={4} data-grid={{ x: 0, y: 1, w: 1, h: 0.5 }} />
-  </GridContainer>;
+  </Grid>;
