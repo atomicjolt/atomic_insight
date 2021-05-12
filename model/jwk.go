d59e9d9a9ab16f5f14535862da27253f2aa8dae0
Carson Fox <carson.fox@atomicjolt.com>
2021-05-11 20:52:33 -0400

    Kid and Pem must be uppercase
    Lowercase fields are not exported, so gopg won't generate columns for
    them.

--- model/jwk.go
+++ model/jwk.go
@@ -2,8 +2,8 @@ package model
 
 type Jwk struct {
 	ID  int64
-	kid string `pg:",notnull"`
-	pem string `pg:",notnull"`
+	Kid string `pg:",notnull"`
+	Pem string `pg:",notnull"`
 
 	ApplicationID int64 `pg:"on_delete:CASCADE"`
 
