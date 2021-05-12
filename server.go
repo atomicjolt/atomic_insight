e3fe379c24ac6d288fafc4426806d647a3abaedd
Carson Fox <carson.fox@atomicjolt.com>
2021-05-12 19:13:45 -0400

    Rename composed middleware variable

--- server.go
+++ server.go
@@ -22,8 +22,8 @@ func main() {
 	insightRepo := repo.NewRepo(db)
 
 	router := controllers.NewRouter(insightRepo, assetsPath)
-	handler := handlers.RecoveryHandler()(handlers.LoggingHandler(os.Stdout, router))
+	middleware := handlers.RecoveryHandler()(handlers.LoggingHandler(os.Stdout, router))
 
 	fmt.Printf("Listening on port %v\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, handler))
+	log.Fatal(http.ListenAndServe(":"+port, middleware))
 }
