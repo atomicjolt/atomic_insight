00b0ff5f1cd683a7bf348b7e9a4bcfa9e7832c19
kylehovey <khoveyc@gmail.com>
2021-05-11 18:36:05 -0700

    Updated configuration to include LTI Advantage Config

--- model/application.go
+++ model/application.go
@@ -1,19 +1,18 @@
 package model
 
-type ApplicationConfig struct{}
-type LtiAdvantageConfig struct{}
+import "github.com/atomicjolt/atomic_insight/config"
 
 type Application struct {
 	ID                    int64
-	Name                  string                 `pg:",notnull"`
-	ClientApplicationName string                 `pg:",notnull"`
-	DefaultConfig         *ApplicationConfig     `pg:"type:jsonb,notnull"`
-	Description           string                 `pg:",notnull"`
-	Key                   string                 `pg:",notnull"`
-	LtiAdvantageConfig    *LtiAdvantageConfig    `pg:",notnull"`
-	ApplicationInstances  []*ApplicationInstance `pg:"rel:has-many"`
-	Jwks                  []*Jwk                 `pg:"rel:has-many"`
-	LtiInstalls           []*LtiInstall          `pg:"rel:has-many"`
+	Name                  string                     `pg:",notnull"`
+	ClientApplicationName string                     `pg:",notnull"`
+	DefaultConfig         *config.ApplicationConfig  `pg:"type:jsonb,notnull"`
+	Description           string                     `pg:",notnull"`
+	Key                   string                     `pg:",notnull"`
+	LtiAdvantageConfig    *config.LtiAdvantageConfig `pg:",notnull"`
+	ApplicationInstances  []*ApplicationInstance     `pg:"rel:has-many"`
+	Jwks                  []*Jwk                     `pg:"rel:has-many"`
+	LtiInstalls           []*LtiInstall              `pg:"rel:has-many"`
 
 	Timestamps
 }
--- model/lti_launch.go
+++ model/lti_launch.go
@@ -1,13 +1,15 @@
 package model
 
+import "github.com/atomicjolt/atomic_insight/config"
+
 type LtiLaunch struct {
 	ID                       int64
-	DeploymentID             string             `pg:",notnull"`
-	Config                   *ApplicationConfig `pg:"type:jsonb,notnull"`
-	ContextID                string             `pg:",notnull"`
-	ResourceLinkID           string             `pg:",notnull"`
-	Token                    string             `pg:",notnull"`
-	ToolConsumerInstanceGuid string             `pg:",notnull"`
+	DeploymentID             string                    `pg:",notnull"`
+	Config                   *config.ApplicationConfig `pg:"type:jsonb,notnull"`
+	ContextID                string                    `pg:",notnull"`
+	ResourceLinkID           string                    `pg:",notnull"`
+	Token                    string                    `pg:",notnull"`
+	ToolConsumerInstanceGuid string                    `pg:",notnull"`
 
 	ApplicationInstanceID int64 `pg:"on_delete:CASCADE"`
 
