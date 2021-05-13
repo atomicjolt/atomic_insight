5826ec614389919085618c8a6d7e461dc069d317
Carson Fox <carson.fox@atomicjolt.com>
2021-05-13 17:08:27 -0400

    Use jwx's key set type, add sig and kid

--- controllers/jwks.go
+++ controllers/jwks.go
@@ -3,26 +3,14 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/lestrrat-go/jwx/jwk"
 )
 
 func (c *ControllerContext) NewJwksController() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		jwks, err := c.Repo.Jwk.All()
+		publicKeys, err := c.Repo.Jwk.PublicJwkSet()
+
 		if err != nil {
 			panic(err)
-		}
-
-		publicKeys := make([]jwk.Key, len(jwks))
-
-		for i, jwk := range jwks {
-			pubKey, err := jwk.PublicKey()
-			if err != nil {
-				panic(err)
-			}
-
-			publicKeys[i] = pubKey
 		}
 
 		enc := json.NewEncoder(w)
