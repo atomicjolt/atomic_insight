0dba329943b7a34b246282f2f07c44565082cb73
kylehovey <khoveyc@gmail.com>
2021-05-14 13:20:56 -0700

    Adding OIDC handler to router

--- controllers/router.go
+++ controllers/router.go
@@ -12,6 +12,7 @@ func NewRouter(repo *repo.Repo, assetsPath string) *mux.Router {
 		AssetsPath: assetsPath,
 	}
 
+	router.HandleFunc("/oidc_init", controllerContext.NewOpenIDInitHandler())
 	router.HandleFunc("/jwks", controllerContext.NewJwksController())
 	router.Handle("/{path:.*}", controllerContext.NewClientFilesHandler())
 
