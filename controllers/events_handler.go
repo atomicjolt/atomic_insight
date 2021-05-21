b8ba1792b554764da8cc8e3e78c85ac512d3b014
Seth <sethduffin@gmail.com>
2021-05-21 09:35:32 -0600

    Removed event handler GET method

--- controllers/events_handler.go
+++ controllers/events_handler.go
@@ -1,82 +1,30 @@
 package controllers
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+	"github.com/atomicjolt/atomic_insight/lib"
 	"io/ioutil"
 	"net/http"
-	"regexp"
-	"strconv"
 )
-
-func writeAndPanic(w http.ResponseWriter, err error) {
-	http.Error(w, err.Error(), http.StatusBadRequest)
-	panic(err)
-}
 
 func (c *ControllerContext) NewEventsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-
-		eventName := vars["name"]
-		eventIdString := vars["id"]
-		var eventId int64
-
-		invalidRequestErr := fmt.Errorf("Invalid request method: %q", r.Method)
-		invalidEventTypeErr := fmt.Errorf("Invalid event type: %q", eventName)
-		invalidEventIdErr := fmt.Errorf("Invalid event id: %d", eventId)
-
 		switch r.Method {
-		case "GET":
-			if eventName == "" {
-				writeAndPanic(w, invalidRequestErr)
-			}
-
-			if eventIdString != "" {
-				n, err := strconv.ParseInt(eventIdString, 10, 64)
-				if err != nil {
-					panic(err)
-				}
-				eventId = n
-			}
-
-			var dataJson []byte
-
-			switch eventName {
-			case "discussion_entry_created":
-				if eventIdString == "" {
-					events, err := c.Repo.DiscussionEntryCreatedEvent.All()
-					if err != nil {
-						panic(err)
-					}
-					dataJson, _ = json.MarshalIndent(events, "", "  ")
-				} else {
-					event, err := c.Repo.DiscussionEntryCreatedEvent.Find(eventId)
-					if err != nil {
-						writeAndPanic(w, invalidEventIdErr)
-					}
-					dataJson, _ = json.MarshalIndent(event, "", "  ")
-				}
-
-			default:
-				writeAndPanic(w, invalidEventTypeErr)
-			}
-
-			fmt.Fprintln(w, string(dataJson))
-
 		case "POST":
-			body, err := ioutil.ReadAll(r.Body)
+			payload, err := ioutil.ReadAll(r.Body)
 			if err != nil {
 				panic(err)
 			}
-			eventName := string(regexp.MustCompile(`"event_name": ?"([^"]*)"`).FindSubmatch(body)[1])
+			eventName, err := lib.EventNameOf(payload)
+			if err != nil {
+				panic(err)
+			}
 
 			switch eventName {
 			case "discussion_entry_created":
-				_, err = c.Repo.DiscussionEntryCreatedEvent.CreateFromPayload(body)
+				err = c.Repo.DiscussionEntryCreatedEvent.CreateFromPayload(payload)
 			default:
-				writeAndPanic(w, invalidEventTypeErr)
+				panic(fmt.Errorf("Invalid event type: %q", eventName))
 			}
 
 			if err != nil {
@@ -84,7 +32,7 @@ func (c *ControllerContext) NewEventsHandler() http.HandlerFunc {
 			}
 
 		default:
-			writeAndPanic(w, invalidRequestErr)
+			panic(fmt.Errorf("Invalid request method: %q", r.Method))
 		}
 	}
 }
