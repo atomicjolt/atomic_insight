a453454a42d65ee8bd84aabf56c7ff5292b749a1
kylehovey <khoveyc@gmail.com>
2021-05-25 11:10:29 -0700

    Organized configuration code into categories

---
+++ config/server.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"log"
+	"sync"
+
+	"github.com/atomicjolt/atomic_insight/lib"
+)
+
+type ServerConfig struct {
+	ServerPort       string `json:"server_port"`
+	DbUser           string `json:"db_user"`
+	DbHost           string `json:"db_host"`
+	Database         string `json:"database"`
+	ClientId         string `json:"client_id"`
+	AuthClientSecret []byte `json:"auth0_client_secret"`
+	AuthClientId     string `json:"auth0_client_id"`
+}
+
+var once sync.Once
+var cachedServerConfig *ServerConfig
+
+func GetServerConfig() *ServerConfig {
+	once.Do(func() {
+		var configs map[string]ServerConfig
+		err := json.Unmarshal(lib.LoadJsonFrom("./server_config.json"), &configs)
+		if err != nil {
+			log.Fatal("Server config file is not valid json: " + err.Error())
+		}
+
+		env := determineEnv()
+		selectedConfig, isPresent := configs[env]
+		if !isPresent {
+			log.Fatal("Server config not found for env: " + env)
+		}
+		cachedServerConfig = &selectedConfig
+
+		base64.StdEncoding.Decode(
+			cachedServerConfig.AuthClientSecret,
+			cachedServerConfig.AuthClientSecret,
+		)
+	})
+
+	return cachedServerConfig
+}
