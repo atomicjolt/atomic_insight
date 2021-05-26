8de5b224642de707269b47ec0b45ee8249f6ccb0
Seth <sethduffin@gmail.com>
2021-05-26 15:52:31 -0600

    Fixed Grid spacing, use layout prop for positioning
    Grid spacing was inconsistent on half sized cards

--- client/src/components/molecules/Grid/Grid.stories.tsx
+++ client/src/components/molecules/Grid/Grid.stories.tsx
@@ -2,16 +2,33 @@ import React from 'react';
 import { Story } from '@storybook/react';
 
 import { Grid } from './Grid';
+import { Card } from '../Card/Card';
 
 export default {
   title: 'Molecules/Grid',
   component: Grid,
 };
 
-export const Default: Story = () =>
-  <Grid title="Default">
-    <div key={1} data-grid={{ x: 0, y: 0, w: 1, h: 1 }} />
-    <div key={2} data-grid={{ x: 1, y: 0, w: 1, h: 1 }} />
-    <div key={3} data-grid={{ x: 2, y: 0, w: 1, h: 1 }} />
-    <div key={4} data-grid={{ x: 0, y: 1, w: 1, h: 0.5 }} />
-  </Grid>;
+const layout = [
+  { i: '1', x: 0, y: 0, w: 1, h: 2 },
+  { i: '2', x: 1, y: 0, w: 1, h: 2 },
+  { i: '3', x: 2, y: 0, w: 1, h: 1 },
+  { i: '4', x: 2, y: 1, w: 1, h: 1 },
+];
+
+export const Default: Story = () => (
+  <Grid title="Default" layout={layout}>
+    <div key={1}>
+      <Card title="Card 1" />
+    </div>
+    <div key={2}>
+      <Card title="Card 2" />
+    </div>
+    <div key={3}>
+      <Card title="Card 3" />
+    </div>
+    <div key={4}>
+      <Card title="Card 4" />
+    </div>
+  </Grid>
+);
--- client/src/components/molecules/Grid/Grid.tsx
+++ client/src/components/molecules/Grid/Grid.tsx
@@ -6,14 +6,26 @@ import './Grid.scss';
 const GridLayout = WidthProvider(Responsive);
 
 export interface GridProps extends React.PropsWithChildren<any> {
-  layout?: [];
+  layout?: any[];
 }
 
-export const Grid = ({ children, layout }: GridProps) => {
-  const [rowHeight, setRowHeight] = useState(10);
+const baseCell = {
+  width: 300,
+  height: 112,
+};
 
-  const cardRatio = 4 / 5; // Ratio of height / width
-  const cardScale = rowHeight / 240; // Ratio of height / standard height
+export const Grid = ({ children, layout }: GridProps) => {
+  const [rowHeight, setRowHeight] = useState(baseCell.height);
+
+  const cardRatio = baseCell.height / baseCell.width; // Ratio of height / width
+  const cardScale = rowHeight / baseCell.height; // Ratio of height / standard height
+
+  // Formated layout
+  const fLayout = layout?.map((card) => {
+    const fCard = card;
+    fCard.i = String(fCard.i);
+    return fCard;
+  }) ?? undefined;
 
   return (
     <GridLayout
@@ -23,13 +35,14 @@ export const Grid = ({ children, layout }: GridProps) => {
       cols={{ lg: 3, md: 3, sm: 3, xs: 3, xxs: 3 }}
       rowHeight={rowHeight}
       isResizable={false}
-      margin={[15, 15]}
-      layout={layout}
+      margin={[16, 16]}
+      layouts={{ lg: fLayout, md: fLayout, sm: fLayout, xs: fLayout, xxs: fLayout }}
       containerPadding={[0, 0]}
-      onWidthChange={(width, margin, cols, padding) => {
-        // Calculates card height by subtracting margins and paddings from the width of container divided by columns
-        const cardHeight = (width - margin[0] - padding[0] * cols) / cols;
-        setRowHeight(cardHeight * cardRatio);
+      onWidthChange={(width, margin, cols) => {
+        // Calculates card height based on card width
+        const cardWidth = (width - (margin[0] * (cols - 1))) / cols;
+        const cardHeight = cardWidth * cardRatio;
+        setRowHeight(cardHeight);
       }}
       children={children}
     />
