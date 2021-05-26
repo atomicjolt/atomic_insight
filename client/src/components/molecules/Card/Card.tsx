eacc83fe884a8c55d7947d2ddeda4c17b03733ff
Seth <sethduffin@gmail.com>
2021-05-26 15:29:10 -0600

    Updated Card styling

--- client/src/components/molecules/Card/Card.tsx
+++ client/src/components/molecules/Card/Card.tsx
@@ -2,29 +2,31 @@ import React  from 'react';
 import './Card.scss';
 
 export enum Impact {
-  Low = "Low Impact",
-  High = "High Impact"
+  Low = 'Low Impact',
+  High = 'High Impact'
 }
 
 export interface CardProps extends React.PropsWithChildren<any> {
   title: string;
   className?: string;
-  impact: Impact;
-  pinned: boolean;
+  impact?: Impact;
+  pinned?: boolean;
   onEdit?: () => void;
   onPin?: (b: boolean) => void;
 }
 
-export const Card = ({ className, children, title, impact, pinned, onEdit, onPin }: CardProps) => {
+export const Card = ({ className, children, title, impact, pinned = false, onEdit, onPin }: CardProps) => {
   return (
     <div
       className={`grid-card ${className || ''}`}
     >
       <div className="card-header">
         <h4 className="title">{title}</h4>
-        <div className="card-impact">
-          <h5 className={`impact-title ${impact === Impact.High ? 'impact-high' : 'impact-low'}`}>{impact}</h5>
-        </div>
+        {impact ? (
+          <div className="card-impact">
+            <h5 className={`impact-title ${impact === Impact.High ? 'impact-high' : 'impact-low'}`}>{impact}</h5>
+          </div>
+        ) : null}
         <div className="card-buttons">
           <button onClick={() => onPin && onPin(!pinned)}>
             <i className={`material-icons${pinned ? '' : '-outlined'}`}>push_pin</i>
