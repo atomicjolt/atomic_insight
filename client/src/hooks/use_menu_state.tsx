311c30ccd092080deaac79b246c22b26ec9c84c7
Seth <sethduffin@gmail.com>
2021-05-26 15:10:11 -0600

    Created use menu state hook

---
+++ client/src/hooks/use_menu_state.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default (defaultValue) => {
+  const [isOpen, setIsOpen] = useState(defaultValue);
+
+  useEffect(() => {
+    const onGlobalClick = () => setIsOpen(false);
+
+    if (isOpen) {
+      window.addEventListener('click', onGlobalClick);
+    } else {
+      window.removeEventListener('click', onGlobalClick);
+    }
+    return () => {
+      window.removeEventListener('click', onGlobalClick);
+    };
+  }, [isOpen]);
+
+  return [isOpen, setIsOpen];
+};
