172a9f4c9f87d4ce48c05e7b9bdca9ff7da225d3
kylehovey <khoveyc@gmail.com>
2021-06-03 13:28:53 -0700

    Added pgstore to the router context

--- controllers/base.go
+++ controllers/base.go
@@ -1,7 +1,11 @@
 package controllers
 
-import "github.com/atomicjolt/atomic_insight/repo"
+import (
+	"github.com/antonlindstrom/pgstore"
+	"github.com/atomicjolt/atomic_insight/repo"
+)
 
 type ControllerContext struct {
-	Repo *repo.Repo
+	Repo  *repo.Repo
+	Store *pgstore.PGStore
 }
