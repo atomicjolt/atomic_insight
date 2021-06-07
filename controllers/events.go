e47b3fc289eda6244e26e2f2ebc39c1221d8f9d1
Carson Fox <carson.fox@atomicjolt.com>
2021-06-07 16:59:29 -0400

    Consolidate error handling using mapstructure

--- controllers/events.go
+++ controllers/events.go
@@ -2,36 +2,34 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/atomicjolt/atomic_insight/middleware"
 	"github.com/atomicjolt/atomic_insight/model"
+	"github.com/mitchellh/mapstructure"
 	"net/http"
 )
 
+type eventsPayload struct {
+	Events []struct {
+		Name    string
+		Payload string
+	}
+}
+
 func (c *ControllerContext) NewEventsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		jwtData := middleware.GetJwtClaims(r)
-		events := jwtData["events"].([]interface{})
+		var payload eventsPayload
+		err := mapstructure.Decode(middleware.GetJwtClaims(r), &payload)
+		if err != nil {
+			panic(err)
+		}
 
-		for _, event := range events {
-			eventMap := event.(map[string]interface{})
-
-			eventName, ok := eventMap["name"]
-			if !ok {
-				panic(fmt.Errorf("missing event name in payload"))
-			}
-
-			eventModel, err := model.EventInstanceFromName(eventName.(string))
+		for _, event := range payload.Events {
+			eventModel, err := model.EventInstanceFromName(event.Name)
 			if err != nil {
 				panic(err)
 			}
 
-			payload, ok := eventMap["payload"]
-			if !ok {
-				panic(fmt.Errorf("missing event data in payload"))
-			}
-
-			if err = json.Unmarshal([]byte(payload.(string)), eventModel); err != nil {
+			if err = json.Unmarshal([]byte(event.Payload), eventModel); err != nil {
 				panic(err)
 			}
 
