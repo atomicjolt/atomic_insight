6d35d696f84129e8761c91f8437f9e81affa2cca
Carson Fox <carson.fox@atomicjolt.com>
2021-06-08 14:57:30 -0400

    Encapsulate messy stuff in jwt validation middleware

--- middleware/jwt_validation.go
+++ middleware/jwt_validation.go
@@ -2,6 +2,8 @@ package middleware
 
 import (
 	"context"
+	"github.com/atomicjolt/atomic_insight/config"
+	"github.com/lestrrat-go/jwx/jwa"
 	"log"
 	"net/http"
 
@@ -9,14 +11,14 @@ import (
 )
 
 //See docs for context.WithValue
-type JwtContextKey int
+type jwtContextKey int
 
 const (
-	EventsContextKey JwtContextKey = iota
-	LtiLaunchParamsKey
+	eventsContextKey jwtContextKey = iota
+	ltiLaunchParamsKey
 )
 
-func NewJwtValidator(next http.Handler, key JwtContextKey, options ...jwt.ParseOption) http.Handler {
+func newJwtValidator(next http.Handler, key jwtContextKey, options ...jwt.ParseOption) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token, err := jwt.ParseRequest(r,
 			options...,
@@ -39,10 +41,28 @@ func NewJwtValidator(next http.Handler, key JwtContextKey, options ...jwt.ParseO
 	})
 }
 
+func EventsJwtValidator(next http.Handler) http.Handler {
+	return newJwtValidator(next,
+		eventsContextKey,
+		jwt.WithFormKey("events"),
+		jwt.WithValidate(true),
+		jwt.WithVerify(jwa.HS256, []byte("shared_secret")),
+	)
+}
+
 func GetEventsPayload(r *http.Request) map[string]interface{} {
-	return r.Context().Value(EventsContextKey).(map[string]interface{})
+	return r.Context().Value(eventsContextKey).(map[string]interface{})
+}
+
+func LtiJwtValidator(next http.Handler) http.Handler {
+	return newJwtValidator(next,
+		ltiLaunchParamsKey,
+		jwt.WithFormKey("state"),
+		jwt.WithValidate(true),
+		jwt.WithKeySet(config.LtiKeySet()),
+	)
 }
 
 func GetLtiLaunchParams(r *http.Request) map[string]interface{} {
-	return r.Context().Value(LtiLaunchParamsKey).(map[string]interface{})
+	return r.Context().Value(ltiLaunchParamsKey).(map[string]interface{})
 }
