70fee0593d8e65ccc712e4dcee4e8d4c7df35b7d
kylehovey <khoveyc@gmail.com>
2021-06-23 20:40:17 -0700

    Added the concept of a launch token with custom claims per-launch

---
+++ lti/launch_token.go
@@ -0,0 +1,71 @@
+package lti
+
+import (
+	"context"
+	"github.com/lestrrat-go/jwx/jwt"
+	"net/http"
+)
+
+type LaunchToken struct {
+	jwt.Token
+}
+
+func NewLaunchToken(idToken IdToken) *LaunchToken {
+	token := jwt.New()
+
+	// TODO: Figure out what values belong in the header
+	token.Set(jwt.SubjectKey, idToken.Subject())
+	token.Set(jwt.AudienceKey, idToken.Audience())
+	token.Set(jwt.IssuerKey, "https://atomicinsight.atomicjolt.xyz")
+	token.Set(jwt.IssuedAtKey, idToken.IssuedAt())
+
+	/**
+	 * This is where state should be forwarded from the ID token
+	 * to the client. This token will be returned on API calls
+	 * to give context to requests.
+	 */
+	token.Set("contextId", idToken.ContextId())
+
+	return &LaunchToken{
+		Token: token,
+	}
+}
+
+func ParseLaunchToken(r *http.Request, options ...jwt.ParseOption) (*LaunchToken, error) {
+	rawToken, err := jwt.ParseRequest(r, options...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	rawClaims, err := rawToken.AsMap(context.Background())
+
+	if err != nil {
+		return nil, err
+	}
+
+	innerClaims := rawClaims["Token"].(map[string]interface{})
+
+	token := jwt.New()
+
+	token.Set(jwt.SubjectKey, innerClaims[jwt.SubjectKey])
+	token.Set(jwt.AudienceKey, innerClaims[jwt.AudienceKey])
+	token.Set(jwt.IssuerKey, innerClaims[jwt.IssuerKey])
+	token.Set(jwt.IssuedAtKey, innerClaims[jwt.IssuedAtKey])
+
+	token.Set("contextId", innerClaims["contextId"])
+
+	return &LaunchToken{
+		Token: token,
+	}, nil
+}
+
+func (t *LaunchToken) ContextId() string {
+	contextId, ok := t.Get("contextId")
+
+	if !ok {
+		panic("Unable to infer context ID from launch token.")
+	}
+
+	return contextId.(string)
+}
