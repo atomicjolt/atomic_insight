afd5a36ab0878a1c4c05a633e683a2de07e4156f
kylehovey <khoveyc@gmail.com>
2021-06-24 15:54:43 -0700

    Added ability to decoded JWT into launch state on client

--- client/src/hooks/use_launch_state.tsx
+++ client/src/hooks/use_launch_state.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
+import jwtDecode from 'jwt-decode';
 
-export interface LaunchState {}
+export interface LaunchState {
+  contextId: string;
+}
+
+interface Wrapped {
+  Token: LaunchState;
+}
+
+const RAW_PAYLOAD: Wrapped = jwtDecode(window.LAUNCH_TOKEN);
+const LAUNCH_STATE: LaunchState = RAW_PAYLOAD.Token;
 
 export default (): LaunchState => {
-  const [launchState] = useState<LaunchState>({} as LaunchState);
+  const [launchState] = useState<LaunchState>(LAUNCH_STATE);
 
   return launchState;
 };
